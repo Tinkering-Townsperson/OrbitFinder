269a9ee57979cd3c59528355dfa1d42a2b0e3938
Paya Maroufi <h2o.Drop2010@gmail.com>
2026-08-08 11:17:24 -0400

    feat: Update compatibility algorithm

--- src/planet.ts
+++ src/planet.ts
@@ -13,7 +13,7 @@ interface Planet {
 
 const mercury: Planet = {
     name: "Mercury",
-    age: 25,
+    age: 4600,
     size: 0.3,
     terrain: 0.3,
     type: "terrestrial",
@@ -26,7 +26,7 @@ const mercury: Planet = {
 
 const venus: Planet = {
     name: "Venus",
-    age: 25,
+    age: 4600,
     size: 0.7,
     terrain: 0.3,
     type: "terrestrial",
@@ -39,7 +39,7 @@ const venus: Planet = {
 
 const earth: Planet = {
     name: "Earth",
-    age: 25,
+    age: 4600,
     size: 0.7,
     terrain: 0.2,
     type: "terrestrial",
@@ -52,7 +52,7 @@ const earth: Planet = {
 
 const mars: Planet = {
     name: "Mars",
-    age: 25,
+    age: 4600,
     size: 0.5,
     terrain: 0.4,
     type: "terrestrial",
@@ -64,10 +64,10 @@ const mars: Planet = {
 }
 
 /* RULE TYPES:
-    - Similarity (difference as close to zero as possible)
-    - Opposite (difference as large as possible)
-    - Adds-to-one (sum as close to one as possible)
-    */
+- Similarity (difference as close to zero as possible)
+- Opposite (difference as large as possible)
+- Adds-to-one (sum as close to one as possible)
+*/
 
 function similarity(trait_a: number, trait_b: number) {
     return 1 - Math.abs(trait_a - trait_b);
@@ -81,22 +81,23 @@ function adds_to_one(trait_a: number, trait_b: number) {
     return trait_a + trait_b;
 }
 
-const SIZE_WEIGHT: number = 0.45;
-const TERRAIN_WEIGHT: number = 0.4;
-const WATER_WEIGHT: number = 0.15;
+const AGE_WEIGHT: number = 0.1;
+const SIZE_WEIGHT: number = 0.4;
+const TERRAIN_WEIGHT: number = 0.3;
+const WATER_WEIGHT: number = 0.1;
+const MOONS_WEIGHT: number = 0.1;
 
 function compatibility(planet1: Planet, planet2: Planet) {
-    const size_compatibility = similarity(planet1.size, planet2.size) * SIZE_WEIGHT;
-    const terrain_compatibility = adds_to_one(planet1.terrain, planet2.terrain) * TERRAIN_WEIGHT;
-    const water_compatibility = opposite(planet1.water, planet2.water) * WATER_WEIGHT;
-
-    return size_compatibility + terrain_compatibility + water_compatibility;
+    const age_compat = (1 - (Math.max(Math.abs(planet1.age - planet2.age) - 2000, 0) / 10000)) * AGE_WEIGHT;
+    const size_compat = similarity(planet1.size, planet2.size) * SIZE_WEIGHT;
+    const terrain_compat = adds_to_one(planet1.terrain, planet2.terrain) * TERRAIN_WEIGHT;
+    const water_compat = opposite(planet1.water, planet2.water) * WATER_WEIGHT;
+    const moon_compat = similarity(planet1.moons/10, planet2.moons/10) * MOONS_WEIGHT;
+    return age_compat + size_compat + terrain_compat + water_compat + moon_compat;
 }
 
-console.log("Earth/Mercury score " + (compatibility(mercury, earth) * 100).toString() + "%")
-console.log()
-console.log("Earth/Venus score " + (compatibility(venus, earth) * 100).toString() + "%");
-console.log()
-console.log("Earth/Mars score " + (compatibility(mars, earth) * 100).toString() + "%");
+console.log("Earth/Mercury score " + Math.round((compatibility(mercury, earth) * 100)).toString() + "%")
+console.log("Earth/Venus score " + Math.round((compatibility(venus, earth) * 100)).toString() + "%");
+console.log("Earth/Mars score " + Math.round((compatibility(mars, earth) * 100)).toString() + "%");
 
 // TODO: Remove console.logs because clutter
