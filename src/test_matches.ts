08743c19e14748cd9d7c0331e3022d183b3c35d8
Paya Maroufi <h2o.Drop2010@gmail.com>
2026-08-08 20:36:18 -0400

    fix: Remove redundant test file

--- src/test_matches.ts
+++
@@ -1,8 +0,0 @@
-import { planets } from "./data/planets-all.ts";
-import {compatibility, type Planet} from "./planet.ts";
-
-for (let i = 0; i < 20; ++i) {
-    const planet1: Planet = planets[Math.floor(Math.random() * planets.length)];
-    const planet2: Planet = planets[Math.floor(Math.random() * planets.length)];
-    console.log("Compatibility of " + planet1.name + " and " + planet2.name + ": " + Math.round(compatibility(planet1, planet2) * 100) + "%");
-}
