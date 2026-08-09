948988ca71b80e09dc84b6b3a98f0ff111526ee4
Wilbur Wei <wilburw2026@gmail.com>
2026-08-09 07:24:03 -0400

    Fix ts unused variable

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from "react";
+// import { useMemo } from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 import { Button } from "./components/Button";
